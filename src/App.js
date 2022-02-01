ce97d3beda4a813d0bddb50957ba482990b03b74
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-02-02 01:58:49 +0530

    add: printingProp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './styles.css';
+import PrintingProps from "./components/PrintingProps";
+// import UsestateBtn from "./components/UseState/UsestateBtn";
+// import UsestateInput from "./components/UseState/UsestateInput";
+// import ComponentTree from "./components/UseState/ComponentTree";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Helow</h1>
+      <PrintingProps name="Hirva" />
+      {/* <UsestateBtn /> */}
+      {/* <UsestateInput /> */}
+      {/* <ComponentTree /> */}
     </div>
   );
 }
